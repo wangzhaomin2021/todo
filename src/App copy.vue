13bee1062b73a2ad30fa62f28122db7c8535b9ef
wangzhaomin <wangzhaomin@qesong.com>
2024-10-22 18:01:12 +0800

    add

---
+++ src/App copy.vue
@@ -0,0 +1,366 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const formInline = reactive({
+  user: "",
+  region: "",
+  date: "",
+});
+
+const onSubmit = () => {
+  console.log("submit!");
+};
+</script>
+
+<template>
+  <el-form :inline="true" :model="formInline" size="small">
+    <!-- 代码 -->
+    <el-row>
+      <el-col :span="2"><span class="title">代码:</span></el-col>
+      <el-col :span="4">
+        <el-form-item label="字体">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <!-- <el-form-item label="间距">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item> -->
+      </el-col>
+      <el-col :span="3">
+        <el-form-item label="加粗">
+          <el-switch v-model="formInline.user"></el-switch>
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="左边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="顶边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 类型 -->
+    <el-row>
+      <el-col :span="2"><span class="title">类型:</span></el-col>
+      <el-col :span="4">
+        <el-form-item label="模板">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="间距">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="3">
+        <el-form-item label="加粗">
+          <el-switch v-model="formInline.user"></el-switch>
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="左边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="顶边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 法人 -->
+    <el-row>
+      <el-col :span="2"><span class="title">法人:</span></el-col>
+      <el-col :span="4">
+        <el-form-item label="模板">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="间距">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="3">
+        <el-form-item label="加粗">
+          <el-switch v-model="formInline.user"></el-switch>
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="左边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="顶边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 【经营】范围 -->
+    <el-row>
+      <el-col :span="2"><span class="title">范围:</span></el-col>
+      <el-col :span="4">
+        <el-form-item label="模板">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="间距">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="3">
+        <el-form-item label="加粗">
+          <el-switch v-model="formInline.user"></el-switch>
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="左边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="顶边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 【注册】资本 -->
+    <el-row>
+      <el-col :span="2"><span class="title">资本:</span></el-col>
+      <el-col :span="4">
+        <el-form-item label="模板">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="间距">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="3">
+        <el-form-item label="加粗">
+          <el-switch v-model="formInline.user"></el-switch>
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="左边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="顶边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 成立【日期】 -->
+    <el-row>
+      <el-col :span="2"><span class="title">成立:</span></el-col>
+      <el-col :span="4">
+        <el-form-item label="模板">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="间距">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="3">
+        <el-form-item label="加粗">
+          <el-switch v-model="formInline.user"></el-switch>
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="左边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="顶边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 经营期限 -->
+    <el-row>
+      <el-col :span="2"><span class="title">期限:</span></el-col>
+      <el-col :span="4">
+        <el-form-item label="模板">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="间距">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="3">
+        <el-form-item label="加粗">
+          <el-switch v-model="formInline.user"></el-switch>
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="左边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="顶边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 组成形式 -->
+    <el-row>
+      <el-col :span="2"><span class="title">组成:</span></el-col>
+      <el-col :span="4">
+        <el-form-item label="模板">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="间距">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="3">
+        <el-form-item label="加粗">
+          <el-switch v-model="formInline.user"></el-switch>
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="左边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="顶边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 个体住所 -->
+    <el-row>
+      <el-col :span="2"><span class="title">个体住所:</span></el-col>
+      <el-col :span="4">
+        <el-form-item label="模板">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="间距">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="3">
+        <el-form-item label="加粗">
+          <el-switch v-model="formInline.user"></el-switch>
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="左边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="顶边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 公司住所 -->
+    <el-row>
+      <el-col :span="2"><span class="title">公司住所:</span></el-col>
+      <el-col :span="4">
+        <el-form-item label="模板">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="间距">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="3">
+        <el-form-item label="加粗">
+          <el-switch v-model="formInline.user"></el-switch>
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="左边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="顶边">
+          <el-input v-model="formInline.user" clearable />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 二维码描述 -->
+    <el-row>
+      <el-col :span="2"><span class="title">二维码描述:</span></el-col>
+      <el-col :span="8">
+        <el-form-item label="模板">
+          <el-input type="textarea" :rows="4" v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="3">
+        <el-form-item label="加粗">
+          <el-switch v-model="formInline.user"></el-switch>
+        </el-form-item>
+      </el-col>
+      <el-col :span="8">
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="字号">
+              <el-input v-model="formInline.user" clearable />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="间距">
+              <el-input v-model="formInline.user" clearable />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="左边">
+              <el-input v-model="formInline.user" clearable />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="顶边">
+              <el-input v-model="formInline.user" clearable />
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+  </el-form>
+</template>
+
+<style scoped>
+.title {
+  font-size: 12px;
+  font-weight: bold;
+  color: #666;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,360 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from "vue";
+
+const formInline = reactive({
+  user: "",
+  region: "",
+  date: "",
+});
+
+const onSubmit = () => {
+  console.log("submit!");
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <el-form :inline="true" :model="formInline" size="small" label-width="76px">
+    <!-- 抬头 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="抬头左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="抬头顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="抬头字号">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 名称 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="名称左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="名称顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="名称字号">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 类型 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="类型左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="类型顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 法人 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="法人左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="法人顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 【经营】范围 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="经营左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="经营顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="经营字号">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 【注册】资本 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="资金左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="资金顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 成立【日期】 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="成立左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="成立顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 经营期限 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="期限左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="期限顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 组成形式 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="组成左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="组成顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 地址 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="地址左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="地址顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="地址字号">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 发证年 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="发证年左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="发证年顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 发证月 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="发证月左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="发证月顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 发证日 -->
+    <el-row>
+      <el-col :span="4">
+        <el-form-item label="发证日左边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+      <el-col :span="4">
+        <el-form-item label="发证日顶边">
+          <el-input v-model="formInline.user" />
+        </el-form-item>
+      </el-col>
+    </el-row>
+
+    <!-- 二维码 -->
+    <el-row>
+      <el-row>
+        <el-col :span="4">
+          <el-form-item label="二维码左边">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="二维码顶边">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="二维码大小">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-row>
+
+    <!-- 公章 -->
+    <el-row>
+      <el-row>
+        <el-col :span="4">
+          <el-form-item label="公章左边">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="公章顶边">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="公章宽度">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="公章高度">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-row>
+
+    <!-- 编号 -->
+    <el-row>
+      <el-row>
+        <el-col :span="4">
+          <el-form-item label="编号内容">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="编号左边">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="编号顶边">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="编号字号">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="编号字体">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="编号加粗">
+            <el-switch v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-row>
+
+    <!-- 提示 -->
+    <el-row>
+      <el-row>
+        <el-col :span="4">
+          <el-form-item label="提示左边">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="提示顶边">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="提示宽度">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="提示高度">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-row>
+
+    <!-- 打印机 -->
+    <el-row>
+      <el-row>
+        <el-col :span="4">
+          <el-form-item label="打印机宽度">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="4">
+          <el-form-item label="打印机高度">
+            <el-input v-model="formInline.user" />
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item label="">
+            <el-radio-group v-model="formInline.user">
+              <el-radio value="A4">A4纸</el-radio>
+              <el-radio value="A3">A3纸</el-radio>
+            </el-radio-group>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-row>
+  </el-form>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.title {
+  font-size: 12px;
+  font-weight: bold;
+  color: #666;
 }
 </style>
